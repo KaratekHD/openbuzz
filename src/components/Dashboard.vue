0586b025bee528606eb0ce652f90cf098efd4e5d
KaratekHD <karatekhd@opensuse.org>
2023-01-02 23:28:31 +0100

    make the login process more smooth

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -4,7 +4,7 @@
       <div class="text-h2">Hallo, {{ auth.student.firstName }} {{ auth.student.lastName }}!</div>
       <v-row>
         <v-col cols="12" md="4">
-          <stats-card @click="router.push('/practice')" />
+          <stats-card @click="router.push('/practice')"/>
         </v-col>
       </v-row>
     </div>
@@ -12,7 +12,40 @@
       <v-dialog fullscreen v-model="logindialog">
         <Login @authed="authed()"/>
       </v-dialog>
-      <v-btn @click="login()" color="primary">Anmelden</v-btn>
+      <v-container class="fill-height">
+        <v-responsive class="d-flex align-center text-center fill-height">
+          <v-img
+            contain
+            height="300"
+            src="@/assets/logo.svg"
+          />
+          <p style="margin-top: 1.5rem"><b>OpenBuzz</b> ist ein alternatives Frontend für drive.buzz Artemis.<br>Um es benutzen zu können, musst du dich
+            mit deinen drive.buzz Zugangsdaten anmelden.<br>Dies ist ein inoffizielles Projekt und steht in keiner
+            Verbindung zu Fahrschulckckpit</p>
+
+          <div class="py-14"/>
+
+          <v-row class="d-flex align-center justify-center">
+            <v-col cols="auto">
+              <v-btn
+                color="primary"
+                min-width="228"
+                size="x-large"
+                variant="flat"
+                @click="login()"
+              >
+                <v-icon
+                  icon="mdi-key"
+                  size="large"
+                  start
+                />
+                Anmelden
+              </v-btn>
+            </v-col>
+
+          </v-row>
+        </v-responsive>
+      </v-container>
     </div>
   </v-container>
 </template>
@@ -28,6 +61,7 @@ let auth = reactive(useAuth())
 let authorized = ref(auth.authorized)
 let logindialog = ref(false)
 const router = useRouter()
+
 function authed() {
   logindialog.value = false
   authorized.value = true
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,45 +1,48 @@
 <template>
-<v-card>
-  <v-container class="fill-height">
-    <v-responsive class="d-flex align-center text-center fill-height">
-      <v-img
-        contain
-        height="300"
-        src="https://go.drive.buzz/img/drivebuzz.cad6c6bb.svg"
-      />
+  <v-card>
+    <v-container class="fill-height">
+      <v-responsive class="d-flex align-center text-center fill-height">
+          <h1 class="text-h2" style="margin-top: 1rem;">Willkommen zurück!</h1>
 
-      <div class="py-14"/>
 
-      <v-row class="d-flex align-center justify-center">
+        <div class="py-14"/>
 
-        <v-form>
-          <v-text-field
-            class="mb-2"
-            label="Email"
-            v-model="email"
-          ></v-text-field>
-          <v-text-field
-            class="mb-2"
-            v-model="password"
-            label="Passwort"
-            type="password"
-          ></v-text-field>
-          <v-btn
-            color="primary"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            variant="flat"
-            @click="signin()"
-          >
-            Anmelden
-          </v-btn>
-        </v-form>
+        <v-row class="d-flex align-center justify-center">
 
-      </v-row>
-    </v-responsive>
-  </v-container>
-</v-card>
+          <v-form>
+            <v-text-field
+              class="mb-2"
+              label="Email"
+              v-model="email"
+            ></v-text-field>
+            <v-text-field
+              class="mb-2"
+              v-model="password"
+              label="Passwort"
+              type="password"
+            ></v-text-field>
+            <v-btn
+              color="primary"
+              min-width="228"
+              rel="noopener noreferrer"
+              size="x-large"
+              variant="flat"
+              @click="signin()"
+              :loading="loading"
+            >
+              <v-icon
+                icon="mdi-login"
+                size="large"
+                start
+              />
+              Anmelden
+            </v-btn>
+          </v-form>
+
+        </v-row>
+      </v-responsive>
+    </v-container>
+  </v-card>
 </template>
 
 <script setup>
@@ -50,7 +53,9 @@ let password = ref("")
 let email = ref("")
 const auth = useAuth()
 const emit = defineEmits(['authed'])
+let loading = ref(false)
 async function signin() {
+  loading.value = true
   await auth.login(email.value, password.value)
   emit('authed')
 }
