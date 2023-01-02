9aa581fbd888b2269c4dcdad3a6de50c10efb0e5
KaratekHD <karatekhd@opensuse.org>
2023-01-02 23:06:59 +0100

    add dark mode

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -4,9 +4,17 @@
     <v-app-bar-title to="/">
       OpenBuzz
     </v-app-bar-title>
+    <template v-slot:append>
+      <v-btn icon @click="$emit('customtheme')">
+        <v-icon v-if="theme.global.name.value === 'dark'" icon="mdi-weather-sunny"/>
+        <v-icon v-else icon="mdi-weather-night"/>
+      </v-btn>
+    </template>
   </v-app-bar>
 </template>
 
 <script setup>
-  //
+import {useTheme} from "vuetify";
+
+const theme = useTheme()
 </script>
--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -19,13 +19,13 @@
         </v-list-item>
         <v-list-item v-if="auth.authorized" to="/practice" color="primary" title="Übungsbogen">
           <template v-slot:prepend>
-            <v-icon icon="mdi-information"/>
+            <v-icon icon="mdi-format-list-checks"/>
           </template>
         </v-list-item>
 
       </v-list>
     </v-navigation-drawer>
-    <default-bar @toggleDrawer="toggleDrawer()"/>
+    <default-bar @customtheme="customTheme()" @toggleDrawer="toggleDrawer()"/>
 
     <default-view/>
   </v-app>
@@ -34,12 +34,35 @@
 <script setup>
 import DefaultBar from './AppBar.vue'
 import DefaultView from './View.vue'
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 import {useAuth} from "@/plugins/auth";
+import {useTheme} from "vuetify";
+
 const auth = useAuth()
 
 let drawer = ref(false)
+const theme = useTheme()
+let useCustomTheme = false
+
+function customTheme() {
+  useCustomTheme = true
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+
 function toggleDrawer() {
   drawer.value = !drawer.value
 }
+
+onMounted(() => {
+  if (!useCustomTheme) {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      theme.global.name.value = "dark"
+    }
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+      if (!useCustomTheme) {
+        theme.global.name.value = event.matches ? "dark" : "light"
+      }
+    });
+  }
+})
 </script>
