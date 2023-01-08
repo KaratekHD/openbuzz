51f277330dac3c702fd760693fc4ee1645c86bde
KaratekHD <karatekhd@opensuse.org>
2023-01-08 13:22:17 +0100

    fix image not being displayed in FREETEXT questions

--- src/components/PracticeQuestion.vue
+++ src/components/PracticeQuestion.vue
@@ -1,76 +1,88 @@
 <template>
-<v-container>
-<v-row v-if="props.question.questionType === 'MULTIPLE_CHOICE'">
-    <v-col v-if="props.question.withPicture" cols="12" md="6">
-      <v-img :src="'https://api.fahrschulcockpit.de/theory-questions/' + props.question.id + '/media'"></v-img>
-    </v-col>
-    <v-col v-if="props.question.withVideo" id="player" cols="12" md="6">
-      <video-player :question="props.question"></video-player>
+    <v-container>
+        <v-row v-if="props.question.questionType === 'MULTIPLE_CHOICE'">
+            <v-col v-if="props.question.withPicture" cols="12" md="6">
+                <v-img :src="'https://api.fahrschulcockpit.de/theory-questions/' + props.question.id + '/media'"></v-img>
+            </v-col>
+            <v-col v-if="props.question.withVideo" id="player" cols="12" md="6">
+                <video-player :question="props.question"></video-player>
+            </v-col>
+            <v-col cols="12" md="6">
+                <div class="text-h5">{{ props.question.paragraph.name }} - {{ props.question.officialNumber }} - Punkte
+                    :
+                    {{ props.question.points }}
+                </div>
+                <v-alert border icon="mdi-help-circle-outline" color="secondary">{{ props.question.title }}</v-alert>
+                {{ props.question.additionalText }}
+                <v-form class="answers">
+                    <table>
+                        <tr v-for="(answer, index) in props.question.options" v-bind:key="index">
+                            <td>
+                                <v-icon v-if="validated" :color="getCheckColor(index)" size=60
+                                        :icon="getCheckType(index)"
+                                        class="answer-icon"></v-icon>
+                            </td>
+                            <td>
+                                <v-checkbox :disabled="validated" v-model="model[index]"
+                                            color="primary" class="answer" :label="answer.text"></v-checkbox>
+                            </td>
+                        </tr>
+                    </table>
+                </v-form>
+                <v-row>
+                    <v-spacer/>
+                    <v-btn class="question-button" :disabled="validated" @click="validateMultipleChoiceQuestion()"
+                           color="primary">
+                        Überprüfen
+                    </v-btn>
+                    <v-btn class="question-button" @click="next()" variant="text">Weiter</v-btn>
+                </v-row>
 
-    </v-col>
-    <v-col cols="12" md="6">
-      <div class="text-h5">{{ props.question.paragraph.name }} - {{ props.question.officialNumber }} - Punkte :
-        {{ props.question.points }}
-      </div>
-      <v-alert border icon="mdi-help-circle-outline" color="secondary">{{ props.question.title }}</v-alert>
-      {{ props.question.additionalText }}
-      <v-form class="answers">
-        <table>
-          <tr v-for="(answer, index) in props.question.options" v-bind:key="index">
-            <td>
-              <v-icon v-if="validated" :color="getCheckColor(index)" size=60 :icon="getCheckType(index)"
-                      class="answer-icon"></v-icon>
-            </td>
-            <td>
-              <v-checkbox :disabled="validated" v-model="model[index]"
-                          color="primary" class="answer" :label="answer.text"></v-checkbox>
-            </td>
-          </tr>
-        </table>
-      </v-form>
-      <v-row>
-        <v-spacer/>
-        <v-btn class="question-button" :disabled="validated" @click="validateMultipleChoiceQuestion()" color="primary">
-          Überprüfen
-        </v-btn>
-        <v-btn class="question-button" @click="next()" variant="text">Weiter</v-btn>
-      </v-row>
 
+            </v-col>
 
-    </v-col>
-
-  </v-row>
-  <v-row v-else-if="props.question.questionType === 'FREE_TEXT'">
-    <v-col cols="12" md="6">
-      <div class="text-h5">{{ props.question.paragraph.name }} - {{ props.question.officialNumber }} - Punkte :
-        {{ props.question.points }}
-      </div>
-      <v-alert border icon="mdi-help-circle-outline" color="secondary">{{ props.question.title }}</v-alert>
-      <v-form class="answers">
-        {{ props.question.questionPattern}}
-        <v-row class="freeTextInput">
-        <v-icon v-if="validated" :color="getFreeTextCheckColor()" size=60 :icon="getFreeTextCheckType()"
-                      class="answer-icon-freetext"></v-icon><v-text-field :disabled="validated" v-model="freetextModel" label="Antwort:"></v-text-field>
         </v-row>
-        <div v-if="validated" class="freeTextInput">Richtige Antwort: {{ props.question.correctAnswer }}</div>
+        <v-row v-else-if="props.question.questionType === 'FREE_TEXT'">
+            <v-col v-if="props.question.withPicture" cols="12" md="6">
+                <v-img :src="'https://api.fahrschulcockpit.de/theory-questions/' + props.question.id + '/media'"></v-img>
+            </v-col>
+            <v-col v-if="props.question.withVideo" id="player" cols="12" md="6">
+                <video-player :question="props.question"></video-player>
+            </v-col>
+            <v-col cols="12" md="6">
+                <div class="text-h5">{{ props.question.paragraph.name }} - {{ props.question.officialNumber }} - Punkte
+                    :
+                    {{ props.question.points }}
+                </div>
+                <v-alert border icon="mdi-help-circle-outline" color="secondary">{{ props.question.title }}</v-alert>
+                <v-form class="answers">
+                    {{ props.question.questionPattern}}
+                    <v-row class="freeTextInput">
+                        <v-icon v-if="validated" :color="getFreeTextCheckColor()" size=60 :icon="getFreeTextCheckType()"
+                                class="answer-icon-freetext"></v-icon>
+                        <v-text-field :disabled="validated" v-model="freetextModel" label="Antwort:"></v-text-field>
+                    </v-row>
+                    <div v-if="validated" class="freeTextInput">Richtige Antwort: {{ props.question.correctAnswer }}
+                    </div>
 
-      </v-form>
-      <v-row class="freeTextInput">
-        <v-spacer />
-        <v-btn class="question-button" :disabled="validated" @click="validateFreeText()" color="primary">
-          Überprüfen
-        </v-btn>
-        <v-btn class="question-button" @click="next()" variant="text">Weiter</v-btn>
-      </v-row>
-    </v-col>
-  </v-row>
-  <v-row v-else>
-    <v-alert type=error>Der Aufgabentyp {{ props.question.questionType }} kann von OpenBuzz nicht angezeigt werden.
-      <v-btn class="question-button" @click="next()">Weiter</v-btn>
-    </v-alert>
+                </v-form>
+                <v-row class="freeTextInput">
+                    <v-spacer/>
+                    <v-btn class="question-button" :disabled="validated" @click="validateFreeText()" color="primary">
+                        Überprüfen
+                    </v-btn>
+                    <v-btn class="question-button" @click="next()" variant="text">Weiter</v-btn>
+                </v-row>
+            </v-col>
+        </v-row>
+        <v-row v-else>
+            <v-alert type=error>Der Aufgabentyp {{ props.question.questionType }} kann von OpenBuzz nicht angezeigt
+                werden.
+                <v-btn class="question-button" @click="next()">Weiter</v-btn>
+            </v-alert>
 
-  </v-row>
-</v-container>
+        </v-row>
+    </v-container>
 
 </template>
 <script setup>
@@ -90,122 +102,123 @@ let freetextModel = ref("")
 
 
 onMounted(() => {
-  if (props.question.questionType === 'MULTIPLE_CHOICE') {
-    for (let i in props.question.options) {
-      model[i] = false
+    if (props.question.questionType === 'MULTIPLE_CHOICE') {
+        for (let i in props.question.options) {
+            model[i] = false
+        }
     }
-  }
 
 
 })
 
 
-
 function validateMultipleChoiceQuestion() {
-  correct_answers = []
-  const correct = props.question.correctAnswer.split(";")
-  let corrects = []
-  for (let i in props.question.options) {
-    if (correct.includes(String(props.question.options[i].position))) {
-      corrects.push(true)
-    } else {
-      corrects.push(false)
+    correct_answers = []
+    const correct = props.question.correctAnswer.split(";")
+    let corrects = []
+    for (let i in props.question.options) {
+        if (correct.includes(String(props.question.options[i].position))) {
+            corrects.push(true)
+        } else {
+            corrects.push(false)
+        }
     }
-  }
-  for (let i in model) {
-    if (model[i] === corrects[i]) {
-      correct_answers.push(true)
-    } else {
-      correct_answers.push(false)
+    for (let i in model) {
+        if (model[i] === corrects[i]) {
+            correct_answers.push(true)
+        } else {
+            correct_answers.push(false)
+        }
     }
-  }
-  success = !correct_answers.includes(false);
-  validated.value = true
+    success = !correct_answers.includes(false);
+    validated.value = true
 }
 
 function getCheckType(i) {
-  if (correct_answers[i]) {
-    return "mdi-checkbox-marked-circle-outline"
-  } else {
-    return "mdi-alert-circle-outline"
-  }
+    if (correct_answers[i]) {
+        return "mdi-checkbox-marked-circle-outline"
+    } else {
+        return "mdi-alert-circle-outline"
+    }
 }
 
 function getCheckColor(i) {
-  if (correct_answers[i]) {
-    return "green"
-  } else {
-    return "red"
-  }
+    if (correct_answers[i]) {
+        return "green"
+    } else {
+        return "red"
+    }
 }
 
 function getFreeTextCheckType(i) {
-  if (success) {
-    return "mdi-checkbox-marked-circle-outline"
-  } else {
-    return "mdi-alert-circle-outline"
-  }
+    if (success) {
+        return "mdi-checkbox-marked-circle-outline"
+    } else {
+        return "mdi-alert-circle-outline"
+    }
 }
 
 function getFreeTextCheckColor(i) {
-  if (success) {
-    return "green"
-  } else {
-    return "red"
-  }
+    if (success) {
+        return "green"
+    } else {
+        return "red"
+    }
 }
+
 function validateFreeText() {
-  if(props.question.correctAnswer === freetextModel.value) {
-    success = true
-  } else {
-    success = false
-  }
-  validated.value = true
+    if (props.question.correctAnswer === freetextModel.value) {
+        success = true
+    } else {
+        success = false
+    }
+    validated.value = true
 }
+
 function next() {
-  if (validated.value) {
-    questions.sendResult(auth, props.question, success)
-  }
-  validated.value = false
-  model.value = []
-  correct_answers = []
-  success = false
-  emit('next')
+    if (validated.value) {
+        questions.sendResult(auth, props.question, success)
+    }
+    validated.value = false
+    model.value = []
+    correct_answers = []
+    success = false
+    emit('next')
 }
 </script>
 
 <style>
 .answer {
-  font-size: 2.5rem;
+    font-size: 2.5rem;
 }
 
 .v-selection-control__input {
-  font-size: 2.5rem;
+    font-size: 2.5rem;
 
 }
 
 .answers {
-  margin-top: 1rem;
+    margin-top: 1rem;
 }
 
 .v-label {
-  font-size: 1rem;
-  margin-left: 1rem;
+    font-size: 1rem;
+    margin-left: 1rem;
 
 }
 
 .answer-icon {
-  position: relative;
-  top: -0.5rem;
-  margin-right: 0.3rem;
+    position: relative;
+    top: -0.5rem;
+    margin-right: 0.3rem;
 }
 
 .freeTextInput {
-  position: relative;
-  top: 1rem;
+    position: relative;
+    top: 1rem;
 }
 
 .question-button {
-  margin-bottom: 1rem;
+    margin-bottom: 1rem;
 }
 </style>
