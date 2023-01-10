ac240933edbb13ce064c93450e02713d81decef5
KaratekHD <karatekhd@opensuse.org>
2023-01-10 13:46:03 +0100

    fix payments not beeing displayed in some cases

--- src/views/BalanceHistory.vue
+++ src/views/BalanceHistory.vue
@@ -1,50 +1,55 @@
 <template>
-    <v-container class="fill-height">
+  <v-container class="fill-height">
 
-        <v-responsive class="fill-height">
-            <div class="text-h2">Mein Konto</div>
+    <v-responsive class="fill-height">
+      <div class="text-h2">Mein Konto</div>
 
-            <v-card variant="outlined">
-                <v-card-title>Kontostand</v-card-title>
-                <v-card-text>
-                    <div :style="'color: ' + color" class="text-h4">
-                        {{ balance }} €
-                    </div>
-                </v-card-text>
-            </v-card>
-            <br>
-            <v-container v-if="loaded">
-                <v-expansion-panels>
+      <v-card variant="outlined">
+        <v-card-title>Kontostand</v-card-title>
+        <v-card-text>
+          <div :style="'color: ' + color" class="text-h4">
+            {{ balance }} €
+          </div>
+        </v-card-text>
+      </v-card>
+      <br>
+      <v-container v-if="loaded">
+        <v-expansion-panels>
 
-                    <v-expansion-panel v-for="(item, index) in payments"
-                                       :color="getColor(item)"
-                                       :title="new Intl.DateTimeFormat('de-DE', {dateStyle: 'long'}).format(new Date(item.date)) + ': ' + item.description">
-                        <v-expansion-panel-text>
-                            <div>
+          <v-expansion-panel v-for="(item, index) in payments"
+                             :color="getColor(item)"
+                             :title="new Intl.DateTimeFormat('de-DE', {dateStyle: 'long'}).format(new Date(item.date)) + ': ' + item.description">
+            <v-expansion-panel-text>
+              <div>
 
-                                <b>Datum: </b>{{
-                                    new Intl.DateTimeFormat('de-DE', {dateStyle: 'long'}).format(new Date(item.date))
-                                }}
-                            </div>
-                            <div>
+                <b>Datum: </b>{{
+                  new Intl.DateTimeFormat('de-DE', {dateStyle: 'long'}).format(new Date(item.date))
+                }}
+              </div>
+              <div v-if="item.amountToPay !== null">
 
-                                <b>Betrag: </b>{{
-                                    item.amountToPay
-                                }} €
-                            </div>
-                            <div v-if="item.educationEventType === 'DRIVING_LESSON'">
-                                <b>Ausbilder: </b>{{
-                                    item.instructor.firstName
-                                }} {{ item.instructor.lastName }}
-                            </div>
+                <b>Betrag: </b>{{
+                  item.amountToPay
+                }} €
+              </div>
+              <div v-else>
+                <b>Betrag: </b>{{
+                  item.amountPaid
+                }} €
+              </div>
+              <div v-if="item.educationEventType === 'DRIVING_LESSON'">
+                <b>Ausbilder: </b>{{
+                  item.instructor.firstName
+                }} {{ item.instructor.lastName }}
+              </div>
 
-                        </v-expansion-panel-text>
-                    </v-expansion-panel>
-                </v-expansion-panels>
-            </v-container>
-        </v-responsive>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-container>
+    </v-responsive>
 
-    </v-container>
+  </v-container>
 </template>
 
 <script setup>
@@ -61,43 +66,43 @@ const theme = useTheme()
 let payments = reactive([])
 let loaded = ref(false)
 onMounted(async () => {
-    await refresh()
+  await refresh()
 })
 
 function getColor(item) {
-    const type = item.educationEventType
-    if (type === "CLASSIC_PAY_PAYMENT") {
-        return "success"
-    }
-    if (type === "DRIVING_LESSON") {
-        return "error"
-    }
-    if (type === "PRODUCT_BOOKING") {
-        return "orange"
-    }
+  const type = item.educationEventType
+  if (type === "CLASSIC_PAY_PAYMENT") {
+    return "success"
+  }
+  if (type === "DRIVING_LESSON") {
+    return "error"
+  }
+  if (type === "PRODUCT_BOOKING") {
+    return "orange"
+  }
 }
 
 async function loadBalance() {
-    const response = await authHelper.getEducation(auth.token)
-    const data = response.data[0]
-    balance.value = data.balancePaid - data.balanceToPay
-    if (balance.value > 0) {
-        color.value = theme.themes.value[theme.global.name.value].colors.success
-    } else {
-        color.value = theme.themes.value[theme.global.name.value].colors.error
-    }
+  const response = await authHelper.getEducation(auth.token)
+  const data = response.data[0]
+  balance.value = data.balancePaid - data.balanceToPay
+  if (balance.value > 0) {
+    color.value = theme.themes.value[theme.global.name.value].colors.success
+  } else {
+    color.value = theme.themes.value[theme.global.name.value].colors.error
+  }
 }
 
 async function loadHistory() {
-    const response = await balanceHelper.getPayments(auth.token)
-    payments = response.data
+  const response = await balanceHelper.getPayments(auth.token)
+  payments = response.data
 }
 
 async function refresh() {
-    loaded.value = false
-    await loadBalance()
-    await loadHistory()
-    loaded.value = true
+  loaded.value = false
+  await loadBalance()
+  await loadHistory()
+  loaded.value = true
 
 }
 </script>
