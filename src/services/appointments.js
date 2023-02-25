65f4fdfd4ce611ef6c4aad3812ba1293813a1286
KaratekHD <karatekhd@opensuse.org>
2023-02-25 12:19:12 +0100

    add copyright notice to every file

--- src/services/appointments.js
+++ src/services/appointments.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2023 KaratekHD. All rights reserverd.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ * This project is not affiliated with drive.buzz or Fahrschulcockpit.
+ */
+
 import api from "@/services/api";
 import balanceHelper from "@/services/balance";
 
