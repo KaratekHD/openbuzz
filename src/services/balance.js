e934b0fa7545496fd336f598aff72436ba033090
KaratekHD <karatekhd@opensuse.org>
2023-02-25 14:27:13 +0100

    remove unused code

--- src/services/balance.js
+++ src/services/balance.js
@@ -21,30 +21,5 @@ export default {
         }
         let endpoint = "/balance/student-education/" + id
         return api().get(endpoint, config)
-    },
-    async calculateTotal(token) {
-        /*
-         *  We could just get this directly from the backend,
-         *  but the value it returns is not always correct.
-         *  So, we will calulate it locally instead.
-         */
-        const endpoint = '/education-events/student/current'
-        const config = {
-            headers: {Authorization: `Bearer ${token}`}
-        }
-        const response = await api().get(endpoint, config)
-        const data = response.data
-        let total = 0
-        for (const i in data) {
-            const item = data[i]
-            const type = item.educationEventType
-            if (type === "CLASSIC_PAY_PAYMENT") {
-                total = total + item.amountPaid
-            }
-            if (type === "DRIVING_LESSON" || type === "PRODUCT_BOOKING") {
-                total = total - item.amountToPay
-            }
-        }
-        return total.toFixed(2)
     }
 }
